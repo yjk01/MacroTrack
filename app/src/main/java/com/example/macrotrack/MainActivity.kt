18a1214b2c6cd3fd27f5a6cc1ddf019a01384e0d
yjk01 <yxk19a@acu.edu>
2024-09-29 20:31:58 -0500

    added topbar

--- app/src/main/java/com/example/macrotrack/MainActivity.kt
+++ app/src/main/java/com/example/macrotrack/MainActivity.kt
@@ -22,13 +22,17 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,10 +60,14 @@ fun MainScreen() {
     var selectedDay by remember { mutableStateOf("") }
     var nutritionDataList by remember { mutableStateOf(List(7) { NutritionData() }) }
 
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { MacroTopAppBar() }
+    ) {paddingValues ->
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(paddingValues)
     ) {
         items(daysOfWeek.size) { index ->
             val day = daysOfWeek[index]
@@ -86,7 +94,7 @@ fun MainScreen() {
             }
         )
     }
-}
+}}
 
 @Composable
 fun DayCard(day: String, nutritionData: NutritionData, onAddDataClick: () -> Unit) {
@@ -220,5 +228,16 @@ fun AddDataDialog(onDismiss: () -> Unit, onSave: (Int, Int, Int, Int) -> Unit) {
     )
 }
 
-val daysOfWeek = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-val foodImages = listOf(R.drawable.img_1, R.drawable.img_2)
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun MacroTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name), // Replace with your app name resource
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
+        modifier = modifier
+    )
+}
